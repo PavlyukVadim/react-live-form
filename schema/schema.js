76f1e9cdfbefd0d44950388239c073bcf948a54e
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-11-07 00:09:36 +0200

    create first schema

--- schema/schema.js
+++ schema/schema.js
@@ -1,7 +1,8 @@
 const graphql = require('graphql');
 const {
   GraphQLObjectType,
-  GraphQLString
+  GraphQLString,
+  GraphQLSchema
 } = graphql;
 
 const UserType = new GraphQLObjectType({
@@ -11,4 +12,30 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     lastName: { type: GraphQLString }
   }
+});
+
+const users = [
+  { id: '23', firstName: 'Walter', lastName: 'White'},
+  { id: '47', firstName: 'Jesse', lastName: 'Pinkman'},
+];
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString }},
+      resolve(parentValue, args) {
+        for (user of users) {
+          if (user.id === args.id) {
+            return user;
+          }
+        }
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
