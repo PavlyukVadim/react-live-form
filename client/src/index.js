b06656dcf850f417d982784e79f0e05b27213b13
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-11-11 20:11:57 +0200

    add grid & basic styles

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,8 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import App from './App';
-import './styles/normalize.css';
+import './stylesheets/normalize.css';
+import './stylesheets/main.scss';
 
 const client = new ApolloClient({
   link: createHttpLink({ uri: 'http://localhost:4000/graphql' }),
