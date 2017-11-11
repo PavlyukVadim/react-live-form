56abfd83bb487ac808bf7a356bb3d546bae9730d
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-11-11 16:40:11 +0200

    add normalize.css

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import App from './App';
+import './styles/normalize.css';
 
 const client = new ApolloClient({
   link: createHttpLink({ uri: 'http://localhost:4000/graphql' }),
