28a621227d0b802257abe4a65a495f7359a1e089
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-11-13 01:21:14 +0200

    change form config

--- client/src/UserApp/containers/TestPage/formConfig.js
+++ client/src/UserApp/containers/TestPage/formConfig.js
@@ -4,10 +4,7 @@ const formConfig = [
     fieldType: 'input',
     dataType: 'int',
     title: 'title for field1',
-    defaultValue: 0,
-    state: {
-      display: true
-    }
+    defaultValue: 0
   },
   {
     name: 'field2',
@@ -15,10 +12,27 @@ const formConfig = [
     dataType: 'int',
     title: 'title for field2',
     defaultValue: 0,
+  },
+  {
+    name: 'field3',
+    fieldType: 'input',
+    dataType: 'int',
+    title: 'title for field3',
+    defaultValue: 0,
     state: {
-      display: 'field1 = 5'
+      value: 'field1 + field2'
     }
   },
+  {
+    name: 'field4',
+    fieldType: 'input',
+    dataType: 'int',
+    title: 'title for field4',
+    defaultValue: 0,
+    state: {
+      value: 'field3 * 2'
+    }
+  }
 ];
 
 export default formConfig;
