3e0a7cd1790a0953c63c7ee59192f7580e131219
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-11-13 01:21:37 +0200

    add expr-eval

--- client/src/UserApp/containers/TestPage/index.js
+++ client/src/UserApp/containers/TestPage/index.js
@@ -5,13 +5,44 @@ import {
   ListSubHeader,
   ListCheckbox
 } from 'react-toolbox/lib/list';
-import { Input } from 'react-toolbox/lib/input';
 import formConfig from './formConfig';
+import { Parser } from 'expr-eval';
+
+var parser = new Parser();
+var expr = parser.parse('2 * x(5) + 1');
+console.log(expr.evaluate({ x: () => 10 })); // 21
 
 const test = {
   id: 1,
   name: 'testName#1',
   description: 'description of test 1'
+};
+
+const hiddenClass = {
+  display: 'none'
+};
+
+const analysisFormDeps = (fields) => {
+  // return fields.map((field) => {
+
+  // }
+};
+
+const getForm = (fields = [], context) => {
+  console.log('context', context);
+  return fields.map((field) => {
+    return (
+      <div>
+        <input
+          type='text'
+          label={field.title}
+          name={field.name}
+          maxLength={16}
+          onChange={(e) => console.log(e)}
+        />
+      </div>
+    );
+  });
 };
 
 class TestPage extends Component {
@@ -25,7 +56,7 @@ class TestPage extends Component {
         <h1>Test: {test.name}</h1>
         Form:
         <div className="formWrapper" style={{backgroundColor: '#ccc', width: '500px'}}>
-          <Input type='text' label='Name' name='name' value={5} maxLength={16} />  
+          {getForm(formConfig, this)}
         </div>
       </div>
     );
