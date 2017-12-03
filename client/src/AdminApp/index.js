e357843ff8f0798e02c587154bf9083a0ccefeec
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-12-03 17:21:31 +0200

    initial AdminApp

--- client/src/AdminApp/index.js
+++ client/src/AdminApp/index.js
@@ -1,9 +1,84 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import {
+  AppBar,
+  Checkbox,
+  IconButton,
+  Layout,
+  NavDrawer,
+  Panel,
+  Sidebar
+} from 'react-toolbox';
+import TestsList from './containers/TestsList';
+import TestPage from './containers/TestPage';
 
-const AdminApp = () => (
-  <div>
-    AdminApp
-  </div>
-);
+class AdminApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      drawerActive: false,
+    };
+    this.toggleDrawerActive = this.toggleDrawerActive.bind(this);
+  }
+
+  toggleDrawerActive() {
+    this.setState((prevState) => {
+      return {
+        drawerActive: !prevState.drawerActive,
+      };
+    });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <NavDrawer
+          active={this.state.drawerActive}
+          onOverlayClick={this.toggleDrawerActive}
+          permanentAt='xxxl'
+        >
+          <p>
+            Navigation, account switcher, etc. go here.
+          </p>
+        </NavDrawer>
+        <Panel>
+          <AppBar leftIcon='menu' onLeftIconClick={this.toggleDrawerActive} />
+          <div className="container">
+            <h1>Main Content</h1>
+            <p>Main content goes here.</p>
+            <Route
+              path='/admin'
+              exact
+              render={
+                () => (
+                  <TestsList 
+                    history={this.props.history}
+                    path={this.props.match.path}
+                  />
+                )
+              }
+            />
+            <Route
+              path='/admin/test/:id'
+              render={
+                () => (
+                  <TestPage/>
+                )
+              }
+            />
+            <Route
+              path='/admin/test/new'
+              render={
+                () => (
+                  <CreateTest/>
+                )
+              }
+            />
+          </div>
+        </Panel>
+      </Layout>
+    );
+  }
+}
 
 export default AdminApp;
