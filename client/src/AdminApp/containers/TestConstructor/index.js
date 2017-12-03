26fe6ca5640516f2c5b4389330f69b2a500007da
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-12-03 18:16:33 +0200

    initial addField

--- client/src/AdminApp/containers/TestConstructor/index.js
+++ client/src/AdminApp/containers/TestConstructor/index.js
@@ -1,44 +1,75 @@
 import React, { Component } from 'react';
 import TestPage from './../../../UserApp/containers/TestPage';
 
-const formConfig = [
-  {
-    name: 'field1',
-    fieldType: 'input',
-    dataType: 'int',
-    title: 'field1',
-    defaultValue: 10,
-  },
-  {
-    name: 'field2',
-    fieldType: 'input',
-    dataType: 'int',
-    title: 'field2',
-    defaultValue: 0,
-  },
-  {
-    name: 'field3',
-    fieldType: 'input',
-    dataType: 'int',
-    title: 'field3',
-    defaultValue: 0,
-    state: {
-      value: 'field1 + field2'
-    }
-  }
-];
-
 class TestConstructor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formConfig: [{
+          name: 'field1',
+          fieldType: 'input',
+          dataType: 'int',
+          title: 'field1',
+          defaultValue: 10,
+        },
+        {
+          name: 'field2',
+          fieldType: 'input',
+          dataType: 'int',
+          title: 'field2',
+          defaultValue: 0,
+        },
+        {
+          name: 'field3',
+          fieldType: 'input',
+          dataType: 'int',
+          title: 'field3',
+          defaultValue: 0,
+          state: {
+            value: 'field1 + field2'
+          }
+        }
+      ]
+    };
+    this.addField = this.addField.bind(this); 
+  }
+
+  addField() {
+    this.setState((prevState) => {
+      const newField = {
+        name: 'field4',
+        fieldType: 'input',
+        dataType: 'int',
+        title: 'field4',
+        defaultValue: 0,
+      };
+      const formConfig = [...prevState.formConfig, newField];
+      return {
+        formConfig
+      };
+    });
+  }
+
   render() {
+    const {
+      formConfig
+    } = this.state;
+
+    console.log('formConfig', formConfig)
+
     return (
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-sm-6">
           <TestPage
             formConfig={formConfig}
           />
         </div>
-        <div className="col-md-6">
-          TestConfig
+        <div className="col-sm-6">
+          <input
+            type="button"
+            value="add field"
+            onClick={this.addField}
+          />
         </div>
       </div>
     );
