f89bb9ba01d5d81c62fdaf474d7d2fe961959275
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-12-13 03:05:38 +0200

    change InitialComponent

--- client/src/App/components/InitialComponent/index.js
+++ client/src/App/components/InitialComponent/index.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import {
+  AppBar,
+  Layout,
+  Panel,
+  Navigation,
+} from 'react-toolbox';
+import './welcome.scss';
 
 const InitialComponent = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to='/signin'>SignIn</Link>    
-      </li>
-      <li>
-        <Link to='/signup'>SignUp</Link>
-      </li>
-    </ul>
-  </div>
+  <Layout>
+    <Panel>
+      <AppBar title="Testing System" leftIcon='check'>
+        <Navigation type="horizontal">
+          <Link className="identification-link" to='/signin'>Sign In</Link>
+          <Link className="identification-link" to='/signup'>Sign Up</Link>
+        </Navigation>
+      </AppBar>
+      <div className="container">
+        <h1>Hi! Welcome to Testing System</h1>
+        <p>Main content for admin goes here.</p>      
+      </div>
+    </Panel>
+  </Layout>
 );
 
 export default InitialComponent;
--- client/src/App/index.js
+++ client/src/App/index.js
@@ -4,12 +4,13 @@ import InitialComponent from './components/InitialComponent';
 import AdminApp from './../AdminApp';
 import UserApp from './../UserApp';
 import Authorization from './containers/Authorization';
-// import Authorization from './containers/Authorization';
+import Registration from './containers/Registration';
 
 const App = () => (
   <div>
     <Route path='/' exact component={InitialComponent} />
     <Route path='/signin' exact component={Authorization} />
+    <Route path='/signup' exact component={Registration} />
     <Route path='/admin' component={AdminApp} />
     <Route path='/user' component={UserApp} />
   </div>
