3a1c44b58c84a089f8b346e9696fa1222d54974f
Vadim Pavlyuk <pavlyuk.dev@gmail.com>
2017-12-25 23:17:31 +0200

    1 element for dashboard

--- client/src/AdminApp/components/Welcome/index.js
+++ client/src/AdminApp/components/Welcome/index.js
@@ -14,16 +14,6 @@ import {
 } from 'recharts';
 import './Welcome.scss';
 
-const data = [
-  {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-  {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-  {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-  {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-  {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-  {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-  {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-];
-
 const TooltipButton = Tooltip(Button);
 
 const Welcome = ({
@@ -32,6 +22,9 @@ const Welcome = ({
 }) => {
   return (
     <div className="row">
+      <div className="row">
+        <h4>Number of users registrations by last 3 years: </h4>
+      </div>
       <LineChart
         width={600} height={300} data={statsData}
         margin={{top: 25, right: 30, left: 20, bottom: 5}}
