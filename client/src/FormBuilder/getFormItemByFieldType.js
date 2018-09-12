c41d8605c139e9880e3226771b9ac0abea2dbd4c
PavlyukVadim <pavlyuk.dev@gmail.com>
2018-09-12 14:57:13 +0300

    clean fieldsConfig

--- client/src/FormBuilder/getFormItemByFieldType.js
+++ client/src/FormBuilder/getFormItemByFieldType.js
@@ -2,6 +2,7 @@ import Input from './FormComponents/Input';
 import Select from './FormComponents/Select';
 import Textarea from './FormComponents/Textarea';
 import Checkbox from './FormComponents/Checkbox';
+
 import './FormComponents/basicStyles.scss';
 
 const kvArray = [
