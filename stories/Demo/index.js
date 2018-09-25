886fa6431fd62b4c4291cbd2ed412128e48d622b
Vadim <pavlyuk.dev@gmail.com>
2018-09-25 21:14:57 +0300

    add stories, delete old example (#21)

---
+++ stories/Demo/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
+
+import LiveForm from 'src/FormBuilder';
+
+const story = storiesOf('Demo', module);
+story.addDecorator(withKnobs);
+story.add(
+  'demo config',
+  withInfo({
+    text: 'basic formConfig for the demo',
+  })(() => {
+
+    const formConfigObj = {
+      formName: 'firstForm',
+      fields: [
+        {
+          name: 'a',
+          fieldType: 'input',
+          dataType: 'int',
+          props: {
+            title: 'field a',
+          },
+          state: {
+            value: {
+              defaultValue: 5,
+            },
+          },
+        },
+        {
+          name: 'b',
+          fieldType: 'input',
+          dataType: 'int',
+          props: {
+            title: 'field b',
+          },
+        },
+        {
+          name: 'c',
+          fieldType: 'input',
+          dataType: 'string',
+          props: {
+            title: 'field c',
+          },
+          state: {
+            value: {
+              defaultValue: 0,
+              valueExpr: 'a + b',
+            },
+          },
+        },
+        {
+          name: 'd',
+          fieldType: 'input',
+          dataType: 'string',
+          props: {
+            title: 'field d',
+          },
+          state: {
+            date: {
+              defaultValue: '30.08.1998',
+              valueFn: 'getDateValue',
+              watch: ['a'],
+            },
+          },
+        },
+        {
+          name: 'e',
+          fieldType: 'input',
+          dataType: 'string',
+          props: {
+            title: 'field e',
+          },
+          state: {
+            value: {
+              defaultValue: 0,
+              valueExpr: 'c * 2',
+            },
+            display: {
+              defaultValue: false,
+              valueExpr: 'a > 10',
+            },
+            disabled: {
+              defaultValue: false,
+              valueExpr: 'a > 34',
+            },
+          },
+        },
+      ],
+      onSubmit: 'submitFunction',
+    };
+
+    const formConfig = object(
+      'formConfig',
+      formConfigObj
+    );
+
+    return (
+      <LiveForm
+        formConfig={formConfig}
+      />
+    )
+  }));
